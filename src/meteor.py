89b0b084e0dd50a27871277821c0ddeb3cfe214d
adityarathi1998 <rathiaditya1998@gmail.com>
2022-04-24 18:46:14 +0530

    Added meteor template file.

---
+++ src/meteor.py
@@ -0,0 +1,17 @@
+import collections
+import itertools
+
+def calculate_harmonic_mean(P, R):
+    pass
+
+
+def calculate_penalty(reference_unigrams, candidate_unigrams):
+    pass
+
+
+def calculate_meteor_score(f_mean, penalty):
+    pass
+
+
+def meteor(candidate, reference):
+    pass
