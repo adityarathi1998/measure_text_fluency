c98d929378d724452ba5e47f7090faf4aae5591b
adityarathi1998 <rathiaditya1998@gmail.com>
2022-04-25 20:22:55 +0530

    Added meteor score template.

--- src/meteor.py
+++ src/meteor.py
@@ -5,7 +5,7 @@ def calculate_harmonic_mean(P, R):
     pass
 
 
-def calculate_penalty(reference_unigrams, candidate_unigrams):
+def calculate_penalty(candidate_unigrams, reference_unigrams):
     pass
 
 
@@ -13,5 +13,43 @@ def calculate_meteor_score(f_mean, penalty):
     pass
 
 
-def meteor(candidate, reference):
+def calculate_unigram_precision_recall(candidate_unigrams, reference_unigrams):
     pass
+
+
+"""
+    Function to convert a sentence into its unigrams.
+    Input:
+        - sentence
+    Output:
+        - List of unigrams.
+"""
+def get_unigrams(sentence):
+    return sentence.split()
+
+
+"""
+    Function to compute meteor score.
+    Input:
+        - candidate: sentenece
+        - reference: sentence
+    Output:
+        - Meteor Score
+"""
+def meteor(candidate, reference):
+    score = 0.0
+    candidate_unigrams = get_unigrams(candidate)
+    reference_unigrams = get_unigrams(reference)
+
+    if (set(candidate_unigrams).intersection(set(reference_unigrams)) == 0):
+        return score
+    
+    # Calculate precision adn recall
+    Precision, Recall = calculate_unigram_precision_recall(candidate_unigrams, reference_unigrams)
+
+    penalty = calculate_penalty(candidate_unigrams, reference_unigrams)
+    f_score =  calculate_harmonic_mean(Precision, Recall)
+    penalty_val = penalty[0]
+    meteor_score = calculate_meteor_score(f_score, penalty_val)
+
+    return meteor_score
