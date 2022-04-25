68b23346ad318ab1ebc8331783d6ebbfbc74d68d
adityarathi1998 <rathiaditya1998@gmail.com>
2022-04-25 20:30:46 +0530

    Added Implementation to calculate meteor score and harmonic mean.

--- src/meteor.py
+++ src/meteor.py
@@ -1,16 +1,38 @@
 import collections
 import itertools
 
+
+"""
+    Calculate the harmonic mean.
+    Input:
+        - P : Unigram precision
+        - R : Unigram Recall
+    Output:
+        - harmonic mean
+"""
 def calculate_harmonic_mean(P, R):
-    pass
+    if R == 0 or P ==0:
+        return 0
+    hm = (10*P*R)
+    hm = hm/float(R + 9*P)
+    return hm
 
 
 def calculate_penalty(candidate_unigrams, reference_unigrams):
     pass
 
 
-def calculate_meteor_score(f_mean, penalty):
-    pass
+"""
+    Function to calculate the meteor score.
+    Input:
+        - f_score
+        - penalty_val
+    Output:
+        - meteor score
+"""
+def calculate_meteor_score(f_score, penalty_val):
+    meteor_score = f_score*(1-penalty_val)
+    return meteor_score
 
 
 def calculate_unigram_precision_recall(candidate_unigrams, reference_unigrams):
